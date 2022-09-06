e6c54efd0c66b6c805eecbe4b27f459b80924723
miguelyunis01 <miguelyunis01@hotmail.com>
2022-09-06 13:59:24 -0500

    se agrego funciones de navegacion pendiente corregir error duplicidad de las categorias

--- src/main.js
+++ src/main.js
@@ -1,4 +1,55 @@
-cont API_KEY = ''
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    Headers: {
+        'content-Type':'aplication/json;charset=uft-8'
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+})
 
 
-console.log('hola');
+async function getTrendingMoviesPreview() {
+    const {data} = await api('trending/movie/day');
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300'+ movie.poster_path,
+        );
+
+            movieContainer.appendChild(movieImg);
+            trendingMoviesPreviewList.appendChild(movieContainer);
+    });
+}
+
+async function getCategoriesPreview() {
+    const {data} = await api('genre/movie/list');
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-title');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id'+ category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreviewList.appendChild(categoryContainer);
+
+    });
+}
